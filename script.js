49752cb841808a669cae5cf227847b81c4bd9339
MikeyBadi <mihaibadia.1999@gmail.com>
2022-04-27 15:38:42 +0200

    added info for the people

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ Scriviamo prima la logica in italiano e poi traduciamo in codice.
 console.log() è nostro amico.
 Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
 */
+
+const teamOffice = [
+  {//angela-caroll-chief-editor
+    fullName: "Angela Caroll",
+    status: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg",
+  },
+  {//angela-lopez-social-media-manager
+    fullName: "Angela Lopez",
+    status: "Social Media Manager",
+    image: "angela-lopez-social-media-manager",
+  },
+  {//barbara-ramos-graphic-designer
+    fullName: "Barbaraa Ramos",
+    status: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+  },
+  {//scott-estrada-developer
+    fullName: "Scott Estrada",
+    status: "Devloper",
+    image: "scott-estrada-developer.jpg",
+  },
+  {//walter-gordon-office-manager
+    fullName: "Walter Gordon",
+    status: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+  {//wayne-barnett-founder-ceo
+    fullName: "Wayne Barnett",
+    status: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  }
+];
+
+console.log(teamOffice);
